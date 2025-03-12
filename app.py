5aa4831d6527ca4e604d444264b84e9c9a47da89
younes_qasempour <qasempouryounes@gmail.com>
2025-03-12 15:24:07 +0330

    Final

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
 from flask import Flask, render_template, request
 import requests
+import smtplib
+from dotenv import load_dotenv
+import os
 
-
+load_dotenv()
 posts = requests.get("https://gist.githubusercontent.com/gellowg/389b1e4d6ff8effac71badff67e4d388/raw/"
                      "fc31e41f8e1a6b713eafb9859f3f7e335939d518/data.json").json()
+
+def send_email(name, email, phone, message):
+    email_message = f"Subject:New Message\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage:{message}"
+    with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+        connection.starttls()
+        connection.login(os.getenv("EMAIL"), os.getenv("PASSWORD"))
+        connection.sendmail(os.getenv("EMAIL"), os.getenv("EMAIL"), email_message)
 
 app = Flask(__name__)
 
@@ -22,6 +32,7 @@ def about():
 def contact():
     if request.method == 'POST':
         data = request.form
+        send_email(data['name'], data['email'], data['phone'], data['message'])
         return render_template("contact.html", msg_sent=True)
     return render_template("contact.html", msg_sent=False)
 
